74945b7f8a13ddcfcdf2e9ebab7d9dec745d5fe0
pele89 <ante.pelivan@gmail.com>
2014-12-21 12:55:07 +0100

    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
 	inv<- NULL
-	set <- function (y){
-		x<<- y
-		inv<<-NULL
+	set <- function (y){ ##defining "set" part of our matrix
+		x<<- y	
+		inv<<-NULL		##inverted matrix is set to be NULL every time we make new cached matrix
 	}
-	get<-function () x
-	setinv<-function () inv<<-solve(x)
-	getinv<- function () inv
-	list(set=set,get=get,setinv=setinv,getinv=getinv)	
+	get <-function () x
+	setinv <-function (invMat) inv<<-invMat ##cacheing our inverted matrix
+	getinv <- function () inv
+	list(set=set,get=get,setinv=setinv,getinv=getinv) ##output of this function is this list of orders making it easy to access our "cache" 	
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
 	inv<-x$getinv
-	if(!is.null(inv)){
+	if(!is.null(inv)){ ##if it isn't NULL inverted matrix had already been cached
 	message ("getting cached data")
-	return (inv)
+	return (inv)		##stopping the execution of the function and returning cached value
 	}
 	data<-x$get()
-	inv<-solve(data,...)
-	x$setinv(inv)
-	inv
+	inv<-solve(data) ##finding the inverted matrix
+	x$setinv(inv)	##storing the inverted matrix to cache
+	inv		##printing the inverted matrix when function is finished
 }
 
-## Return a matrix that is the inverse of 'x'cacheca
+
